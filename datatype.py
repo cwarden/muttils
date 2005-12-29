6eda8ca25f354a622a31033b7057dfb47ff8826c
chris <devnull@localhost>
2005-12-29 16:51:37 +0000

    Removed bang. Nicer error handling.

--- datatype.py
+++ datatype.py
@@ -1,23 +1,24 @@
-#! /usr/bin/env python
-# $Id: datatype.py,v 1.2 2005/02/04 16:30:24 chris Exp $
+# $Id: datatype.py,v 1.3 2005/12/29 17:51:37 chris Exp $
 
 import os.path
 import urllib
 
-def Usage(path):
+def fileErr(path):
 	import sys
-	sn = os.path.basename(sys.argv[0])
-	print '%s: %s: not a regular file.' % (sn, path)
-	sys.exit(2)
+	exe = os.path.basename(sys.argv[0])
+	sys.exit('%s: %s: not a regular file.' % (exe, path))
 
 def dataType(f):
 	path = os.path.abspath(os.path.expanduser(f))
-	if not os.path.isfile(path): Usage(path)
+	if not os.path.isfile(path): fileErr(path)
 	# urllib uses the deprecated mimelib
 	# but email does not work for type detection
 	# on local files
-	fp = urllib.urlopen('file://%s' % path)
-	type = fp.info().gettype()
-	data = fp.read()
-	fp.close()
+	try:
+		fp = urllib.urlopen('file://%s' % path)
+		type = fp.info().gettype()
+		data = fp.read()
+	finally: fp.close()
 	return data, type
+
+# EOF vim:ft=python
