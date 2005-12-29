33fc10b90a3039ab9b3cf3d036354db1e02413ed
chris <devnull@localhost>
2005-12-29 16:50:10 +0000

    Cleaned up imports. Made errors slimmer. Removed executable parts and _test.

--- Urlcollector.py
+++ Urlcollector.py
@@ -1,20 +1,11 @@
-#!/usr/bin/env python
+# $Id: Urlcollector.py,v 1.6 2005/12/29 17:50:10 chris Exp $
 
-Urlcollector_rcsid = '$Id: Urlcollector.py,v 1.5 2005/12/17 11:51:32 chris Exp $'
-
-import os, re, sys
+import os.path, re, sys
 from tpager.LastExit import LastExit
-from cheutils.Rcsparser import Rcsparser
 from Urlregex import Urlregex
 from datatype import dataType
 
-rcs = Rcsparser(Urlcollector_rcsid)
-
-def inputError():
-	print
-	print rcs.getVals(shortv=True)
-	print 'needs file arguments or standard input'
-	sys.exit(2)
+def exNam(): return os.path.basename(sys.argv[0])
 
 class Urlcollector(Urlregex, LastExit):
 	"""
@@ -29,19 +20,21 @@ class Urlcollector(Urlregex, LastExit):
 		self.nt = False         # needs terminal
 
 	def parseError(self):
-		print rcs.getVals(shortv=True)
-		errmsg = 'Encountered malformed html!\n' \
+		errmsg = '%s: encountered malformed html!\n' \
 			 'Might be unable to retrieve every url.\n' \
-			 'Continue? [Yes], no '
+			 'Continue? [Yes], no ' % exNam()
 		if self.nt: LastExit.termInit(self)
 		yorn = raw_input(errmsg)
 		if self.nt: LastExit.reInit(self)
-		if yorn: sys.exit()
+		if yorn in ('n', 'N'): sys.exit()
 
 	def urlCollect(self):
 		if not self.files: # read from stdin
 			try: data = sys.stdin.read()
-			except KeyboardInterrupt: inputError()
+			except KeyboardInterrupt:
+				err = '\n%s: needs stdin or filename(s)' \
+						% exNam()
+				sys.exit(err)
 			Urlregex.findUrls(self, data)
 		else:
 			for f in self.files:
@@ -49,31 +42,10 @@ class Urlcollector(Urlregex, LastExit):
 				Urlregex.findUrls(self, data, type)
 		if self.ugly: self.parseError()
 		if self.pat and self.items:
-			try: self.pat = re.compile(r'%s' % self.pat, re.IGNORECASE)
-			except re.error, strerror: Usage(strerror)
-			self.items = filter(lambda i: self.pat.search(i), self.items)
-
-
-def _test():
-	sourcefile = sys.argv[0]
-	print rcs.getVals(shortv=True)
-	print """
-hello world, these are 3 urls:
-cis.tarzisius.net
-www.python.org.
-<www.black
-trash.org>
-they are contained in the source file %s
-you are currently testing.
-I will read myself now so to speak and
-collect the urls:
-	""" % sourcefile
-	ur = Urlcollector()
-	ur.files = [sourcefile]
-	ur.urlCollect()
-	print ur.items
-#        os.unlink(tf)
-	
-if __name__ == '__main__': _test()
-
-# EOF vim:ft=python
+			try: self.pat = re.compile(r'%s' % self.pat, re.I)
+			except re.error, e:
+				err = '%s: %s in pattern %s' \
+						% (exNam(), e, self.pat)
+				sys.exit(err)
+			self.items = filter(lambda i: self.pat.search(i),
+					self.items)
