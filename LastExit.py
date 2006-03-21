1457f5e941884197266f129a5ce9accc58b5224d
Christian Ebert <blacktrash@gmx.net>
2006-03-21 17:25:17 +0100

    Read and write in binary mode

--- LastExit.py
+++ LastExit.py
@@ -12,10 +12,10 @@ class Termplus:
 		self.dev = os.ctermid()
 
 	def write(self, o):
-		readwrite.writeFile(self.dev, o)
+		readwrite.writeFile(self.dev, o, mode="wb")
 	
 	def readline(self, size=-1):
-		return readwrite.readLine(self.dev, size=size)
+		return readwrite.readLine(self.dev, mode="rb", size=size)
 
 
 class LastExit(Termplus):
