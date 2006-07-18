1e4b1baeb0bb407eb66f31da7605c12dec181c5e
Christian Ebert <blacktrash@gmx.net>
2006-07-18 17:14:08 +0200

    4 spaces instead of tabs

--- datatype.py
+++ datatype.py
@@ -4,11 +4,11 @@ import urllib2
 from cheutils import filecheck
 
 def dataType(path):
-	path = filecheck.fileCheck(path, spec='isfile', absolute=True)
-	try:
-		fp = urllib2.urlopen('file://%s' % path)
-		kind = fp.info().gettype()
-		data = fp.read()
-	finally:
-		fp.close()
-	return data, kind
+    path = filecheck.fileCheck(path, spec='isfile', absolute=True)
+    try:
+        fp = urllib2.urlopen('file://%s' % path)
+        kind = fp.info().gettype()
+        data = fp.read()
+    finally:
+        fp.close()
+    return data, kind
