0076940e92cdb90537b5af4493cb15d83308faff
Christian Ebert <blacktrash@gmx.net>
2006-07-18 12:26:05 +0200

    4 spaces instead of tabs

--- Urlparser.py
+++ Urlparser.py
@@ -4,115 +4,114 @@ import email, email.Utils, re
 from email.Errors import MessageParseError
 
 protos = ('all', 'web',
-		'http', 'ftp', 'gopher',
-		'mailto',
-		'mid')
+        'http', 'ftp', 'gopher',
+        'mailto',
+        'mid')
 # finger, telnet, whois, wais?
 
 # header tuples (to be extended)
 searchkeys = ('subject', 'organization',
-	      'user-agent', 'x-mailer', 'x-newsreader',
-	      'list-id', 'list-subscribe', 'list-unsubscribe',
-	      'list-help', 'list-archive', 'list-url',
-	      'mailing-list', 'x-habeas-swe-9')
+              'user-agent', 'x-mailer', 'x-newsreader',
+              'list-id', 'list-subscribe', 'list-unsubscribe',
+              'list-help', 'list-archive', 'list-url',
+              'mailing-list', 'x-habeas-swe-9')
 
-refkeys = ('references', 'in-reply-to', 'message-id',
-	   'original-message-id')
+refkeys = ('references', 'in-reply-to', 'message-id', 'original-message-id')
 
 addrkeys = ('from', 'to', 'reply-to', 'cc',
-	    'sender', 'x-sender', 'mail-followup-to',
-	    'x-apparently-to',
-	    'errors-to', 'x-complaints-to', 'x-beenthere')
+            'sender', 'x-sender', 'mail-followup-to',
+            'x-apparently-to',
+            'errors-to', 'x-complaints-to', 'x-beenthere')
 
 quote_re = re.compile(r'^(> ?)+', re.MULTILINE)
 
 def msgFactory(fp):
-	try:
-		return email.message_from_file(fp)
-	except MessageParseError:
-		return ''
+    try:
+        return email.message_from_file(fp)
+    except MessageParseError:
+        return ''
 
 def unQuote(s):
-	return quote_re.sub('', s)
+    return quote_re.sub('', s)
 
 
 class UrlparserError(Exception):
-	'''Exception class for this module.'''
+    '''Exception class for this module.'''
 
 class Urlparser(object):
-	'''
-	Subclass of Urlregex.
-	Extracts urls from html text
-	messages or mailboxes.
-	'''
-	def __init__(self, proto='all'):
-		self.proto = proto
-		self.url_re = None
-		self.items = []
-		self.msg = ''
+    '''
+    Subclass of Urlregex.
+    Extracts urls from html text
+    messages or mailboxes.
+    '''
+    def __init__(self, proto='all'):
+        self.proto = proto
+        self.url_re = None
+        self.items = []
+        self.msg = ''
 
-	def protoTest(self):
-		if not self.proto in protos:
-			err = "`%s': invalid spec, use one of %s" \
-					% (self.proto, ', '.join(protos))
-			raise UrlparserError, err
+    def protoTest(self):
+        if not self.proto in protos:
+            err = "`%s': invalid spec, use one of %s" \
+                    % (self.proto, ', '.join(protos))
+            raise UrlparserError, err
 
-	def headParser(self, keys):
-		for key in keys:
-			vals = self.msg.get_all(key)
-			if vals:
-				pairs = email.Utils.getaddresses(vals)
-				urls = [pair[1] for pair in pairs if pair[1]]
-				self.items += urls
+    def headParser(self, keys):
+        for key in keys:
+            vals = self.msg.get_all(key)
+            if vals:
+                pairs = email.Utils.getaddresses(vals)
+                urls = [pair[1] for pair in pairs if pair[1]]
+                self.items += urls
 
-	def headSearcher(self):
-		for key in searchkeys:
-			vals = self.msg.get_all(key, [])
-			for val in vals:
-				urls = [u[0] for u in self.url_re.findall(val)]
-				self.items += urls
+    def headSearcher(self):
+        for key in searchkeys:
+            vals = self.msg.get_all(key, [])
+            for val in vals:
+                urls = [u[0] for u in self.url_re.findall(val)]
+                self.items += urls
 
-	def mailDeconstructor(self, s):
-		'''Checks if given string is message or mailbox.
-		If no, returns string.
-		Parses message/mailbox for relevant headers
-		adding urls to list of items and returns
-		text parts for further searching.'''
-		try:
-			self.msg = email.message_from_string(s)
-		except MessageParseError:
-			return s
-		if not self.msg or not self.msg['Message-ID']:
-			return s
-		# else it's a message or a mailbox
-		if not self.msg.get_unixfrom():
-			sl = self.msgDeconstructor()
-		else: # treat s like a mailbox because it might be one
-			from cStringIO import StringIO
-			from mailbox import PortableUnixMailbox
-			sl = [] # list of strings to search
-			fp = StringIO()
-			fp.write(s)
-			mbox = PortableUnixMailbox(fp, msgFactory)
-			while self.msg != None:
-				self.msg = mbox.next()
-				if self.msg:
-					sl = self.msgDeconstructor(sl)
-			fp.close()
-		s = '\n'.join(sl)
-		return unQuote(s) # get quoted urls spanning more than 1 line
+    def mailDeconstructor(self, s):
+        '''Checks if given string is message or mailbox.
+        If no, returns string.
+        Parses message/mailbox for relevant headers
+        adding urls to list of items and returns
+        text parts for further searching.'''
+        try:
+            self.msg = email.message_from_string(s)
+        except MessageParseError:
+            return s
+        if not self.msg or not self.msg['Message-ID']:
+            return s
+        # else it's a message or a mailbox
+        if not self.msg.get_unixfrom():
+            sl = self.msgDeconstructor()
+        else: # treat s like a mailbox because it might be one
+            from cStringIO import StringIO
+            from mailbox import PortableUnixMailbox
+            sl = [] # list of strings to search
+            fp = StringIO()
+            fp.write(s)
+            mbox = PortableUnixMailbox(fp, msgFactory)
+            while self.msg != None:
+                self.msg = mbox.next()
+                if self.msg:
+                    sl = self.msgDeconstructor(sl)
+            fp.close()
+        s = '\n'.join(sl)
+        return unQuote(s) # get quoted urls spanning more than 1 line
 
-	def msgDeconstructor(self, sl=None):
-		if sl == None:
-			sl = []
-		if self.proto != 'mid':
-			if self.proto in ('all', 'mailto'):
-				self.headParser(addrkeys)
-			self.headSearcher()
-		else:
-			self.headParser(refkeys)
-		for part in self.msg.walk(): # use email.Iterator?
-			if part.get_content_maintype() == 'text':
-				text = part.get_payload(decode=True)
-				sl.append(text)
-		return sl
+    def msgDeconstructor(self, sl=None):
+        if sl == None:
+            sl = []
+        if self.proto != 'mid':
+            if self.proto in ('all', 'mailto'):
+                self.headParser(addrkeys)
+            self.headSearcher()
+        else:
+            self.headParser(refkeys)
+        for part in self.msg.walk(): # use email.Iterator?
+            if part.get_content_maintype() == 'text':
+                text = part.get_payload(decode=True)
+                sl.append(text)
+        return sl
