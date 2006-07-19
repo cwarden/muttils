14d675a9b11d3d29ea916a929b7755b3f13ca88e
Christian Ebert <blacktrash@gmx.net>
2006-07-20 01:05:04 +0200

    4 spaces instead of tabs

--- terminfo.py
+++ terminfo.py
@@ -10,17 +10,17 @@ of current terminal.
 
 osname = os.uname()[0]
 if osname == 'Darwin':
-	dev = os.ctermid()
-	tt = systemcall.backQuote(['stty', '-f', '%s' % dev, '-a'])
-	attribs = tt.split()
-	t_rows = int(attribs[3])
-	t_cols = int(attribs[5])
+    dev = os.ctermid()
+    tt = systemcall.backQuote(['stty', '-f', '%s' % dev, '-a'])
+    attribs = tt.split()
+    t_rows = int(attribs[3])
+    t_cols = int(attribs[5])
 elif osname == 'Linux':
-	tt = systemcall.backQuote(['stty', '-F', '/dev/tty', '-a'])
-	attribs = tt.split('; ')
-	t_rows = int(attribs[1].split()[1])
-	t_cols = int(attribs[2].split()[1])
+    tt = systemcall.backQuote(['stty', '-F', '/dev/tty', '-a'])
+    attribs = tt.split('; ')
+    t_rows = int(attribs[1].split()[1])
+    t_cols = int(attribs[2].split()[1])
 
 def _test():
-	print 'This terminal has %d rows and %d columns.' \
-		% (t_rows, t_cols)
+    print 'This terminal has %d rows and %d columns.' \
+        % (t_rows, t_cols)
