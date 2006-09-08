c6766e01f5a722e2700c4d98df3d5af04f6fde89
Christian Ebert <blacktrash@gmx.net>
2006-09-08 09:30:27 +0200

    Try statement after declaring `fp'

--- datatype.py
+++ datatype.py
@@ -5,8 +5,8 @@ from cheutils import filecheck
 
 def dataType(path):
     path = filecheck.fileCheck(path, spec='isfile', absolute=True)
+    fp = urllib2.urlopen('file://%s' % path)
     try:
-        fp = urllib2.urlopen('file://%s' % path)
         kind = fp.info().gettype()
         data = fp.read()
     finally:
