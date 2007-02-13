eddc9373ce40ceb8569b4d6d8a28a3d68424aa88
Christian Ebert <blacktrash@gmx.net>
2007-02-13 04:35:58 +0100

    urlpager: implement optparse

--- muttils/urlpagercommand.py
+++ muttils/urlpagercommand.py
@@ -9,86 +9,74 @@
 # input is checked anew for each file.
 ###
 
-import usage, urlpager, util
-import getopt, os.path, sys
+import urlpager, version
+from urlregex import valid_protos
+import optparse, sys
 
-optstring = 'bd:D:f:hiIlM:np:k:r:tw:x'
+proginfo = 'Urlpager - search, choose and retrieve url'
 
-urlpager_help = '''
-[-p <protocol>][-r <pattern>][-t][-x][-f <ftp client>][<file> ...]
--w <download dir> [-r <pattern]
--i [-M <filemask>][-r <pattern>][-k <mbox>][<file> ...]
--I [-M <filemask>][-r <pattern>][-k <mbox>][<file> ...]
--l [-I][-M <filemask>][-r <pattern>][-k <mbox>][<file> ...]
--d <mail hierarchy>[:<mail hierarchy>[:...]] \\
-        [-I][-l][-M <filemask>][-r <pattern>][-k <mbox>][<file> ...]
--D <mail hierarchy>[:<mail hierarchy>[:...]] \\
-        [-I][-l][-M <filemask>][-r <pattern>][-k <mbox>][<file> ...]
--n [-r <pattern][-I][-l][-k <mbox>][<file> ...]
--b [-r <pattern][-I][<file> ...]
--h (display this help)'''
-
-def userhelp(error='', i=False):
-    usage.usage(help=urlpager_help, err=error, interrupt=i)
-
+progdesc = '''Search files or standard input for urls,
+choose 1 url interactively and retrieve it.
+Urls are either web locations or Message-IDs.'''
 
 def run():
-    '''Command interface to urlpager.'''
+    parser = optparse.OptionParser(formatter=optparse.TitledHelpFormatter(),
+            usage='%prog [option] [files]', description=progdesc,
+            version=version.version_(proginfo))
+    parser.set_defaults(midrelax=False, specdirs=None,
+            proto='all', pat=None, xb=False, tb=False, getdir='', ftp='',
+            local=False, browse=False, news=False, mhiers=None, mspool=True)
 
-    opts = {}
+    parser.add_option('-p', '--protocol', dest='proto',
+            type='choice', choices=valid_protos,
+            help='narrow down url choice to protocol PROTO')
+    parser.add_option('-r', '--regex', dest='pat',
+            help='narrow down url choice to urls matching PAT')
+    parser.add_option('-x', '--xbrowser', dest='xb', action='store_true',
+            help='prefer x11-browser')
+    parser.add_option('-t', '--textbrowser', dest='tb', action='store_true',
+            help='prefer textbrowser')
+    parser.add_option('-w', '--wget', dest='getdir',
+            help='download chosen url to directory GETDIR using wget')
+    parser.add_option('-f', '--ftp',
+            help='use ftp client FTP for ftp urls')
+    parser.add_option('-i', '--midrelax', action='store_true',
+            help='choose from undeclared message-ids (false positives probable)')
+    parser.add_option('-l', '--local', action='store_true',
+            help='search for chosen message only locally')
+    parser.add_option('-b', '--browse', action='store_true',
+            help='view chosen message at google groups with browser')
+    parser.add_option('-n', '--news', action='store_true',
+            help='news only: do not search local mailboxes')
+    parser.add_option('-m', '--mbox', dest='kiosk',
+            help='append message chosen by id to mbox KIOSK')
+    parser.add_option('-d', '--dirs', dest='mhiers',
+            help='search for message in directories MHIERS (colon-separated list, including mail spool)')
+    parser.add_option('-D', '--specdirs',
+            help='search for message in directories SPECDIRS (colon-separated list, excluding mail spool)')
+    parser.add_option('-M', '--mask',
+            help='exclude mailboxes matching MASK from search')
+
+    options, args = parser.parse_args()
+
+    for o in [options.midrelax, options.news, options.local, options.browse,
+            options.kiosk, options.mhiers, options.specdirs, options.mask]:
+        if o:
+            options.proto = 'mid'
+            break
+
+    if options.midrelax:
+        options.decl = False
+    if options.specdirs:
+        options.mhiers = options.specdirs
+        options.mspool = False
+    del options.midrelax, options.specdirs
+
     try:
-        sysopts, opts['files'] = getopt.getopt(sys.argv[1:], optstring)
-
-        for o, a in sysopts:
-            if o == '-b': # don't look up msgs locally
-                opts['proto'] = 'mid'
-                opts['browse'] = True
-            if o == '-d': # specific mail hierarchies
-                opts['proto'] = 'mid'
-                opts['mhiers'] = a.split(':')
-            if o == '-D': # specific mail hierarchies, exclude mspool
-                opts['proto'] = 'mid'
-                opts['mspool'] = False
-                opts['mhiers'] = a.split(':')
-            if o == '-f': # ftp client
-                opts['ftp'] = a
-            if o == '-h':
-                userhelp()
-            if o == '-I': # look for declared message-ids
-                opts['proto'] = 'mid'
-                opts['decl'] = True
-            if o == '-i': # look for ids, in text w/o prot (email false positives)
-                opts['proto'] = 'mid'
-            if o == '-k': # mailbox to store retrieved message
-                opts['proto'] = 'mid'
-                opts['kiosk'] = a
-            if o == '-l': # only local search for message-ids
-                opts['proto'] = 'mid'
-                opts['local'] = True
-            if o == '-M': # file mask
-                opts['proto'] = 'mid'
-                opts['mask'] = a
-            if o == '-n': # don't search mailboxes for message-ids
-                opts['proto'] = 'mid'
-                opts['mhiers'] = None
-            if o == '-p': # protocol(s)
-                opts['proto'] = a
-            if o == '-r': # regex pattern to match urls against
-                opts['pat'] = a
-            if o == '-x': # xbrowser
-                opts['xb'] = True
-            if o == '-t': # text browser command
-                opts['tb'] = True
-            if o == '-w': # download dir for wget
-                if not os.path.isdir(util.absolutepath(a)):
-                    userhelp('%s: not a directory' % a)
-                opts['proto'] = 'web'
-                opts['getdir'] = a
-
-        u = urlpager.urlpager(opts=opts)
+        u = urlpager.urlpager(files=args, opts=options.__dict__)
+        parser.destroy()
         u.urlsearch()
-
-    except (getopt.GetoptError, urlpager.UrlpagerError), e:
-        userhelp(e)
+    except urlpager.UrlpagerError, inst:
+        sys.exit(inst)
     except KeyboardInterrupt:
-        userhelp('needs filename(s) or stdin', i=True)
+        sys.exit('need filenames or standard input')
