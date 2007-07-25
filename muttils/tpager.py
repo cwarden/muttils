035ff3e2ce54835c874e1a4b4d0b4feb7791fcbb
Christian Ebert <blacktrash@gmx.net>
2007-07-25 15:58:30 +0200

    tpager: horizontal resize; only resize when connected to terminal
    Avoid error when trying to resize without term connection.
    Use fd attribute to check for resize.

--- muttils/tpager.py
+++ muttils/tpager.py
@@ -8,48 +8,46 @@ try:
 except ImportError:
     pass
 
-def valclamp(x, low, high):
-    '''Clamps x between low and high.'''
-    return max(low, min(x, high))
-
-
 class tpager(object):
     '''
     Customizes interactive choice to current terminal.
     '''
     pages =  {}         # dictionary of pages
-    resize = False      # do we resize?
-    fd = None
+    fd = None           # file descriptor reference for resizing term
     rows = 0            # terminal $LINES
     cols = 0            # terminal $COLUMNS
     itemsdict = {}      # dictionary of items to choose
     ilen = 0            # length of items' list
     more = False        # more than 1 page
+    plen = 0            # how many pages
 
     def __init__(self, ui, items=None, name='item',
                  format='sf', ckey='', qfunc='quit', crit='pattern'):
         self.ui = ui
         self.items = items or [] # (text) items to choose from
         self.name = name         # general name of an item
-        if format in ('sf', 'bf'):
-            self.format = format # sf: simple format, bf: bracket format
-        else:
+        self.format = format
+        if self.format not in ('sf', 'bf'):
             raise util.DeadMan('%s: invalid format, use one of "sf", "bf"'
-                    % format)
-        if not ckey or not ckey in 'qQ-':
-            self.ckey = ckey     # key to customize pager
-        else:
-            raise util.DeadMan("the `%s' key is internally reserved." % ckey)
+                               % self.format)
+        self.ckey = ckey         # key to customize pager
+        if self.ckey in ('q', 'Q', '-'):
+            raise util.DeadMan("the `%s' key is internally reserved."
+                               % self.ckey)
         self.qfunc = qfunc       # name of exit function
         self.crit = crit         # name of criterion for customizing
 
-    def resizehandler(self, signalnum, frame):
+    def ttysize(self):
         arrini = fcntl.ioctl(self.fd, termios.TIOCGWINSZ, '\0' * 8)
         self.rows, self.cols = array.array('h', arrini)[:2]
         # rows: retain 1 line for header + 1 for menu
         # cols need 1 extra when lines are broken
         self.rows -= 1
         self.cols += 1
+
+    def resizehandler(self, signalnum, frame):
+        self.ttysize()
+        self.pagesdict()
 
     def terminspect(self):
         '''Get current term's columns and rows, return customized values.'''
@@ -61,63 +59,62 @@ class tpager(object):
                     return 0
             return 0
 
-        notty = False # assume connection to terminal
+        notty = False  # assume connection to terminal
+        self.fd = None # reset if class was not reloaded (ckey)
         for dev in (sys.stdout, sys.stdin):
             try:
                 fd = dev.fileno()
                 istty =  os.isatty(fd)
                 if not istty:
                     notty = True
-                elif not self.resize:
+                elif not self.fd:
                     self.fd = fd
-                    self.resizehandler(None, None)
-                    signal.signal(signal.SIGWINCH, self.resizehandler)
-                    self.resize = True
             except ValueError:
                 # I/O operation on closed file
                 notty = True
             except NameError:
                 pass
-        if not self.resize:
+        try:
+            if self.fd and not notty:
+                self.resizehandler(None, None)
+                signal.signal(signal.SIGWINCH, self.resizehandler)
+            else:
+                self.ttysize()
+        except NameError:
             self.rows = (_gettyenv('LINES') or 24) - 1
             self.cols = (_gettyenv('COLUMNS') or 80) + 1
+        if notty:
+            self.fd = None
         return notty
-
-    def addpage(self, buff, lines, pn):
-        '''Adds a page to pages and returns pageno.'''
-        pn += 1
-        if self.more:
-            # fill page with newlines
-            buff += '\n' * (self.rows-lines-1)
-        self.pages[pn] = buff
-        return pn
 
     def formatitems(self):
         '''Formats items of itemsdict to numbered list.'''
-        if not self.items:
-            return []
-
-        def simpleformat(key):
-            '''Simple format of choice menu,
-            recommended for 1 line items.'''
+        def _simpleformat(key):
             return '%s) %s\n' % (key.rjust(maxl), self.itemsdict[key])
-        def bracketformat(key):
-            '''Format of choice menu with items
-            that are longer than 1 line.'''
+
+        def _bracketformat(key):
             return '[%s]\n%s\n' % (key, self.itemsdict[key])
 
-        formdict = {'sf': simpleformat, 'bf': bracketformat}
+        if not self.items:
+            return []
+        formfunc = (_simpleformat, _bracketformat)[self.format=='bf']
         self.ilen = len(self.items)
         ikeys = [str(i) for i in xrange(1, self.ilen+1)]
         self.itemsdict = dict(zip(ikeys, self.items))
-        if self.format == 'sf':
-            maxl = len(ikeys[-1])
-        formatfunc = formdict[self.format]
-        return [formatfunc(k) for k in ikeys]
+        maxl = len(ikeys[-1])
+        return [formfunc(k) for k in ikeys]
 
     def pagesdict(self):
         '''Creates dictionary of pages to display in terminal window.
         Keys are integers as string starting from "1".'''
+        def _addpage(buff, lines, pn):
+            pn += 1
+            if self.more:
+                # fill page with newlines
+                buff += '\n' * (self.rows-lines-1)
+            self.pages[pn] = buff
+            return pn
+
         self.itemsdict.clear()
         self.pages.clear()
         # all this still supposes that no wrapped text item
@@ -134,9 +131,10 @@ class tpager(object):
                 lines = linecheck
             else:
                 self.more = True
-                pn = self.addpage(buff, lines, pn)
+                pn = _addpage(buff, lines, pn)
                 buff, lines = item, ilines
-        pn = self.addpage(buff, lines, pn)
+        pn = _addpage(buff, lines, pn)
+        self.plen = len(self.pages)
 
     def coltrunc(self, s, cols=0):
         '''Truncates string at beginning by inserting '>'
@@ -155,6 +153,9 @@ class tpager(object):
 
     def pagemenu(self):
         '''Lets user page through a list of items and make a choice.'''
+        def _valclamp(x, low, high):
+            return max(low, min(x, high))
+
         header = self.coltrunc('*%s*\n' % util.plural(self.ilen, self.name),
                                self.cols - 2)
         if not self.more:
@@ -174,18 +175,17 @@ class tpager(object):
         else: # more than 1 page
             # switch paging command according to paging direction
             pds = {-1: 'back', 1: 'forward'}
-            plen = len(self.pages)
             pn = 1 # start at first page
             pdir = -1 # initial paging direction reversed
             while True:
                 bs = '' # reverse paging direction
-                if plen > 1:
-                    if 1 < pn < plen:
+                if self.plen > 1:
+                    if 1 < pn < self.plen:
                         bs = '-:%s, ' % pds[pdir*-1]
                     else:
                         pdir *= -1
                     menu = ('page %d of %d [%s], ^C:cancel, %sq:%s'
-                            % (pn, plen, pds[pdir], bs, self.qfunc))
+                            % (pn, self.plen, pds[pdir], bs, self.qfunc))
                 else:
                     # items selected by self.crit might fit on 1 page
                     menu = '^C:cancel, q:%s' % self.qfunc
@@ -194,10 +194,10 @@ class tpager(object):
                 menu += ', number '
                 reply = self.pagedisplay(header, menu, pn)
                 if not reply:
-                    pn = valclamp(pn+pdir, 1, plen)
+                    pn = _valclamp(pn+pdir, 1, self.plen)
                 elif bs and reply == '-':
                     pdir *= -1
-                    pn = valclamp(pn+pdir, 1, plen)
+                    pn = _valclamp(pn+pdir, 1, self.plen)
                 elif reply in self.itemsdict:
                     self.items = [self.itemsdict[reply]]
                     break
@@ -221,6 +221,6 @@ class tpager(object):
             retval, self.items = '', None
         if notty:
             it.reinit()
-        if self.resize:
+        if self.fd:
             signal.signal(signal.SIGWINCH, signal.SIG_DFL)
         return retval
