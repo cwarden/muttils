aa2f48658554d38911ce659a1574ab7ebb75c5ad
Christian Ebert <blacktrash@gmx.net>
2009-07-07 18:14:18 +0200

    tpager: use generator expression in sum()
    OK with Python 2.4

--- muttils/tpager.py
+++ muttils/tpager.py
@@ -79,7 +79,7 @@ class tpager(object):
         for item in self.formatitems():
             # lines of item, taking overruns into account
             ilines = item.splitlines()
-            ilines = sum([len(line)/self.cols for line in ilines], len(ilines))
+            ilines = sum((len(line)/self.cols for line in ilines), len(ilines))
             linecheck = lines + ilines
             if linecheck < self.rows:
                 buff += item
